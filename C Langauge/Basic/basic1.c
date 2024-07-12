a67cc43e823a141962753385b0c7513d55df7561
Pushkar Modi <pushkarmodi111@gmail.com>
2024-07-12 23:15:32 +0530

    Basic

---
+++ C Langauge/Basic/basic1.c
@@ -0,0 +1,23 @@
+// -> Single line comment
+/*
+        Multi line comment
+*/
+
+// * -> Astrick operator
+
+
+//  headerfile
+#include <stdio.h>
+#include <conio.h>
+
+//  main function
+int main()
+{  // body of the function 
+    printf("Hello World");
+    printf(" Yesha Modi");
+    printf("\nI am persuing Btech in CSE");
+    printf("\n\ntwentythousand\t\t\t20000");
+    
+    return 0;    
+}
+
